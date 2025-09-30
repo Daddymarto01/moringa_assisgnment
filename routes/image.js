6fc56238c051a2bda10b86b7384a277b4b17658b
Martin Odongo <daddymarto01@gmail.com>
2025-09-30 23:46:59 +0300

    override

--- routes/image.js
+++ routes/image.js
@@ -1,34 +1,42 @@
 const express = require('express');
 const router = express.Router();
-let Image = require('../models/images');
+const Image = require('../models/images');
 
-router.get('/:id', (req,res)=>{
-    // console.log(req);
-    Image.findById(req.params.id,function(err, image){
-        if (err) console.log(err)
-        // console.log(image);
-        res.render('singleImage', {title: 'Single Image', image:image})
-    } )
-})
+// 🆕 ADD THIS ROUTE to render index.ejs with image data
+router.get('/', async (req, res) => {
+  try {
+    const images = await Image.find({});
+    res.render('index', { images }); // 👈 now your EJS has access to 'images'
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Images failed to pull!');
+  }
+});
 
-router.put('/:id', (req,res) =>{
-    console.log(req.params.id)
-    console.log(req.body);
-    Image.findOneAndUpdate({_id:req.params.id},{
-        name:req.body.name
-    },{new: true}, function(err,image ){
-        if (err) console.log(err)
-        res.redirect('/')
-    })
-})
+router.get('/:id', (req, res) => {
+  Image.findById(req.params.id, function (err, image) {
+    if (err) console.log(err);
+    res.render('singleImage', { title: 'Single Image', image: image });
+  });
+});
 
-router.delete('/:id', (req,res) =>{
-    console.log(req.params.id)
+router.put('/:id', (req, res) => {
+  Image.findOneAndUpdate(
+    { _id: req.params.id },
+    { name: req.body.name },
+    { new: true },
+    function (err, image) {
+      if (err) console.log(err);
+      res.redirect('/');
+    }
+  );
+});
 
-    Image.deleteOne({_id: req.params.id}, function(err){
-        if (err) console.log(err)
-        res.redirect('/index')
-    })
-})
+router.delete('/:id', (req, res) => {
+  Image.deleteOne({ _id: req.params.id }, function (err) {
+    if (err) console.log(err);
+    res.redirect('/');
+  });
+});
 
-module.exports = router
+module.exports = router;
